272ae69a9ba069021e612f59965c3868be1e1367
arjun-baidya <arjun.chandra.baidya@gmail.com>
2021-12-14 17:36:08 +0600

    update

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Col, Container, Image, Row } from 'react-bootstrap'
+import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap'
 import './projects.css'
 
 function Projects() {
@@ -32,13 +32,42 @@ function Projects() {
                     </Col>
                     <Col>
                     <Card className="card-section">
-                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Img variant="top" src="./bike-pro-img.png" fluid />
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title>Bike Project</Card.Title>
                         <Card.Text>
-                            This is a longer card with supporting text below as a natural
-                            lead-in to additional content. This content is a little bit longer.
+                            This a bike project which is a front-end project which is deployed on github.
+                            Made by Html and CSS.
                         </Card.Text>
+                        <Button  href='https://arjun-baidya.github.io/honda-collection/' target='blank' variant="primary">Live</Button>
+                        <Button  className='code-btn' href='https://github.com/arjun-baidya/honda-collection' target='blank' variant="primary">Code</Button>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                    <Card className="card-section">
+                        <Card.Img variant="top" src="./panda-ecom-pro-img.png" fluid/>
+                        <Card.Body>
+                        <Card.Title>Panda E-commerce</Card.Title>
+                        <Card.Text>
+                            This is a e-commerce project which is a front-end project.
+                            Made by Html and CSS.
+                        </Card.Text>
+                        <Button  href='https://arjun-baidya.github.io/panda-ecommerce/' target='blank' variant="primary">Live</Button>
+                        <Button  className='code-btn' href='https://github.com/arjun-baidya/panda-ecommerce' target='blank' variant="primary">Code</Button>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                    <Card className="card-section">
+                        <Card.Img variant="top" src="./yoga-pro-img.png" />
+                        <Card.Body>
+                        <Card.Title>Yoga Project</Card.Title>
+                        <Card.Text>
+                            This is a React based front-end project.
+                        </Card.Text>
+                        <Button  href='https://traditional-yoga.netlify.app/' target='blank' variant="primary">Live</Button>
+                        <Button  className='code-btn' href='https://github.com/arjun-baidya/yoga' target='blank' variant="primary">Code</Button>
                         </Card.Body>
                     </Card>
                     </Col>
@@ -51,30 +80,8 @@ function Projects() {
                             This is a longer card with supporting text below as a natural
                             lead-in to additional content. This content is a little bit longer.
                         </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </Col>
-                    <Col>
-                    <Card className="card-section">
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a longer card with supporting text below as a natural
-                            lead-in to additional content. This content is a little bit longer.
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </Col>
-                    <Col>
-                    <Card className="card-section">
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a longer card with supporting text below as a natural
-                            lead-in to additional content. This content is a little bit longer.
-                        </Card.Text>
+                        <Button  href='https://arjun-baidya.github.io/panda-ecommerce/' target='blank' variant="primary">Live</Button>
+                        <Button  className='code-btn' href='https://github.com/arjun-baidya/panda-ecommerce' target='blank' variant="primary">Code</Button>
                         </Card.Body>
                     </Card>
                     </Col>
@@ -87,6 +94,8 @@ function Projects() {
                             This is a longer card with supporting text below as a natural
                             lead-in to additional content. This content is a little bit longer.
                         </Card.Text>
+                        <Button  href='https://arjun-baidya.github.io/panda-ecommerce/' target='blank' variant="primary">Live</Button>
+                        <Button  className='code-btn' href='https://github.com/arjun-baidya/panda-ecommerce' target='blank' variant="primary">Code</Button>
                         </Card.Body>
                     </Card>
                     </Col>
